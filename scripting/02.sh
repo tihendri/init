7cbcd1f0a77d90fe597bb61b8b50a1ad633ffa8e
tihendri <tihendri@student.wethinkcode.co.za>
2021-02-07 12:21:38 +0200

    restructured, more readable

--- scripting/02.sh
+++ scripting/02.sh
@@ -1,13 +1,19 @@
-if [ $# -gt 0 ]
+echo "This script will delete an active user on the VM"
+echo "================================================\n"
+echo "\n"
+echo "Current Active Users:"
+echo "====================="
+cat /etc/passwd | grep home | gawk -F':' '{print $1}'
+echo "\n"
+read -p "Enter the username to delete: "  username
+if [ $username ]
 then
-        sudo deluser $1
-    echo " "
-    echo "$1 is no longer in the list of existing users"
-    echo " "
-    echo "New list of existing users"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    cat /etc/passwd | grep home | gawk -F':' '{print $1}'
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        sudo deluser $username
 else
-        echo "Error: Please run the script with the users name you want to remove..."
+        echo "Error: No username specified"
 fi
+echo "\n"
+echo "Active Users:"
+echo "============="
+cat /etc/passwd | grep home | gawk -F':' '{print $1}'
+echo "\n"
